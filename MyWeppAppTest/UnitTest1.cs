fc88e0223a1f9dc1cdb32af0073efe65e3302c82
Dikshasoft <vijaymishra26@gmail.com>
2017-01-21 14:21:19 +0530

    added test project

---
+++ MyWeppAppTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyWeppAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, 10);
+        }
+    }
+}
